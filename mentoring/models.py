bc4ce3281bb6fa4a6ac45c15a072a1aa55bd1e28
Torham <torhamdev@gmail.com>
2023-03-14 19:57:07 +0330

    Add mentoring database models

--- mentoring/models.py
+++ mentoring/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from utils.models import BaseModel
 
-# Create your models here.
+
+class MentoringRequest(BaseModel):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    offered_price = models.BigIntegerField()
+    user_description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MentroingPage(BaseModel):
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return f"{self.description[0:60]}..."
