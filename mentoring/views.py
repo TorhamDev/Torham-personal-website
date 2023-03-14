301af673d15df6c39fb5b0d5c14e0154ff7d00d2
Torham <torhamdev@gmail.com>
2023-03-14 20:04:31 +0330

    Make mentoring page daynamic

--- mentoring/views.py
+++ mentoring/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render
 from django.views import View
+from mentoring.models import MentroingPage
+
+
+def get_mentoring_page_description():
+    description = MentroingPage.objects.all().last()
+    if description is None:
+        return "Oops, there is not text :("
+
+    return description.description
 
 
 class MentoringPage(View):
 
     def get(self, request):
-
+        description = get_mentoring_page_description()
         return render(
             request=request,
             template_name="mentoring.html",
-            context={}
+            context={
+                "description": description,
+            }
         )
 
     def post(self, request):
-
+        description = get_mentoring_page_description()
         return render(
             request=request,
             template_name="mentoring.html",
             context={
                 "message": "sended",
+                "description": description,
             }
         )
