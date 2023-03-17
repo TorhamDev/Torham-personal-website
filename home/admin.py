0f004a53f0e55a64aa5e429146ab98015d1641e3
Torham <torhamdev@gmail.com>
2023-03-17 12:46:55 +0330

    Add about page databse model to admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from home.models import IndexPage, ProjectPage
+from home.models import IndexPage, ProjectPage, AboutPage
 
 # Register your models here.
 admin.site.register(IndexPage)
 admin.site.register(ProjectPage)
+admin.site.register(AboutPage)
