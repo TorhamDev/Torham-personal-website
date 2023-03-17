c174ef4a4dd8e1d020c81b15828f16086b7d7719
Torham <torhamdev@gmail.com>
2023-03-17 12:30:28 +0330

    Fix index page loading daa problem

--- home/views/index.py
+++ home/views/index.py
@@ -7,11 +7,21 @@ class IndexView(View):
 
     def get(self, request):
         index_page_data = IndexPage.objects.all().last()
+        if index_page_data != None:
+            context = {
+                "main_text_source_link": index_page_data.main_text_source_link,
+                "main_text_source": index_page_data.main_text_source,
+                "main_text": index_page_data.main_text
+            }
+        else:
+            context = {
+                "main_text_source_link": "#",
+                "main_text_source": "torham",
+                "main_text": "Hello, World!",
+            }
 
         return render(
             request=request,
             template_name="index.html",
-            context={
-                "index_data": index_page_data,
-            }
+            context=context
         )
