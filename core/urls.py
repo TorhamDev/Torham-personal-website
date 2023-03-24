7cb71e4a8df84ccc91751eabfec50bb0a40e62c5
Torham <torhamdev@gmail.com>
2023-03-24 11:43:07 +0330

    Add media url

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
     path('mentoring/', include("mentoring.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
