4cc6d10c45abc1c361754a41bcfad0800929e5e1
dori-dev <mr.dori.dev@gmail.com>
2023-03-26 11:21:37 +0330

    Add podcast model

---
+++ home/models/podcast.py
@@ -0,0 +1,33 @@
+from django.db import models
+from utils.models import BaseModel
+from django.core.validators import FileExtensionValidator
+
+
+class Podcast(BaseModel):
+    name = models.CharField(
+        max_length=100,
+        help_text="podcast name",
+    )
+    cover = models.ImageField(
+        upload_to="covers/",
+        help_text="podcast cover",
+    )
+    description = models.TextField(
+        help_text='podcast description',
+    )
+    file = models.FileField(
+        upload_to='audio/',
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=[
+                    'mp3',
+                    'wav',
+                    'qcp',
+                ]
+            )
+        ],
+        help_text='podcast audio file',
+    )
+
+    def __str__(self) -> str:
+        return self.name
