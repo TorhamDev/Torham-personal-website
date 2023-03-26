3ed4057bdaa1c0ccc2cdc8a13a424fe504a05b6f
dori-dev <mr.dori.dev@gmail.com>
2023-03-27 00:04:00 +0330

    Add search input

--- home/views/podcast.py
+++ home/views/podcast.py
@@ -1,12 +1,30 @@
 from django.views import generic
+from django.db.models import Q
 
 from home.models import Podcast
+from home import forms
 
 
-class PodcastList(generic.ListView):
+class BasePodcastView(generic.ListView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = forms.SearchForm(self.request.GET)
+        return context
+
+
+class PodcastList(BasePodcastView):
     model = Podcast
     context_object_name = 'podcasts'
-    paginate_by = 5
+    paginate_by = 4
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('search')
+        if query:
+            queryset = queryset.filter(
+                Q(description__icontains=query) | Q(name__icontains=query),
+            ).distinct()
+        return queryset
 
     def get_template_names(self):
         if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
