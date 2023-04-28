f0986d715dccddce61c0ea07c522e69006cce94e
Torham <torhamdev@gmail.com>
2023-04-28 22:30:50 +0330

    get CSRF_TRUSTED_ORIGINS value from env

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [os.environ.get("CSRF_TRUSTED_ORIGINS").split(",")]
 
 # Application definition
 LOCAL_APPS = [
