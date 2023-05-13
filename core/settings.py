a1da37503ba767514a1bfb3b867737e04290e787
dori-dev <mr.dori.dev@gmail.com>
2023-05-13 16:13:55 +0330

    add podcast templates

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "abcd")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
